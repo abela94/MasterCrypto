c1a790cfdea87d6fbe24cb936f7cd3b79864cabc
abela94 <abelbk007@gmail.com>
2025-01-01 07:34:15 +0300

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,10 @@
-// import Hero from '@/components/Hero'
-// import MarketOverview from '@/components/MarketOverview'
-// import FeatureHighlights from '@/components/FeatureHighlights'
-// import CallToAction from '@/components/CallToAction'
-// import Testimonials from '@/components/Testimonials'
-// import SubscriptionPopup from '@/components/SubscriptionPopup'
-// import FeedbackForm from '@/components/FeedbackForm'
-
-// export default function Home() {
-//   return (
-//     <div>
-//       <Hero />
-//       <MarketOverview />
-//       <FeatureHighlights />
-
-//       <Testimonials />
-//       <FeedbackForm />
-//       <CallToAction />
-//       <SubscriptionPopup />
-//     </div>
-//   )
-// }
-
 import Hero from '@/components/Hero'
 import MarketOverview from '@/components/MarketOverview'
 import FeatureHighlights from '@/components/FeatureHighlights'
 import CallToAction from '@/components/CallToAction'
 import Testimonials from '@/components/Testimonials'
+import SubscriptionPopup from '@/components/SubscriptionPopup'
 import FeedbackForm from '@/components/FeedbackForm'
-import dynamic from 'next/dynamic'
-
-// Dynamically import components that use client-side features
-const SubscriptionPopup = dynamic(() => import('@/components/SubscriptionPopup'), {
-  ssr: false
-})
 
 export default function Home() {
   return (
@@ -40,6 +12,7 @@ export default function Home() {
       <Hero />
       <MarketOverview />
       <FeatureHighlights />
+
       <Testimonials />
       <FeedbackForm />
       <CallToAction />
@@ -47,4 +20,31 @@ export default function Home() {
     </div>
   )
 }
+
+// import Hero from '@/components/Hero'
+// import MarketOverview from '@/components/MarketOverview'
+// import FeatureHighlights from '@/components/FeatureHighlights'
+// import CallToAction from '@/components/CallToAction'
+// import Testimonials from '@/components/Testimonials'
+// import FeedbackForm from '@/components/FeedbackForm'
+// import dynamic from 'next/dynamic'
+
+// // Dynamically import components that use client-side features
+// const SubscriptionPopup = dynamic(() => import('@/components/SubscriptionPopup'), {
+//   ssr: false
+// })
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <Hero />
+//       <MarketOverview />
+//       <FeatureHighlights />
+//       <Testimonials />
+//       <FeedbackForm />
+//       <CallToAction />
+//       <SubscriptionPopup />
+//     </div>
+//   )
+// }
 
