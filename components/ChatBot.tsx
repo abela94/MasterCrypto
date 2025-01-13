b22a215ffff1b0ebd9768a6dca5fcbe4ae0b366a
abela94 <abelbk007@gmail.com>
2025-01-13 17:41:55 +0300

    ai feature addded and localhost fixed and http problem

--- components/ChatBot.tsx
+++ components/ChatBot.tsx
@@ -29,7 +29,7 @@ export default function ChatBot() {
       setInput('')
       
       try {
-        const response = await fetch('http://mastercrypto.org/chat/', {
+        const response = await fetch('https://mastercrypto.org/chat/', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ message: input })
