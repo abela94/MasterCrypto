27f4fede3616038888a9fa089a04f06c771c9dce
abela94 <abelbk007@gmail.com>
2025-01-19 11:46:33 +0300

    Update route.ts

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,28 +1,42 @@
-import { NextResponse } from 'next/server'
+// import { NextResponse } from 'next/server'
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-  try {
-    const response = await fetch(`https://mastercrypto.org/posts/${params.id}/`)
-    const data = await response.json()
-    return NextResponse.json(data)
-  } catch (error) {
-    return NextResponse.json({ error: 'Failed to fetch blog post' }, { status: 500 })
-  }
-}
-
-// export async function generateStaticParams() {
+// export async function GET(request: Request, { params }: { params: { id: string } }) {
 //   try {
-//     // Fetch all post IDs from your API
-//     const response = await fetch('https://mastercrypto.org/posts/')
-//     const posts = await response.json()
-
-//     // Return an array of objects, each with an 'id' property
-//     return posts.map((post: { id: string }) => ({
-//       id: post.id,
-//     }))
+//     const response = await fetch(`https://mastercrypto.org/posts/${params.id}/`)
+//     const data = await response.json()
+//     return NextResponse.json(data)
 //   } catch (error) {
-//     console.error('Failed to fetch post IDs for static generation:', error)
-//     return [] // Return an empty array if fetching fails
+//     return NextResponse.json({ error: 'Failed to fetch blog post' }, { status: 500 })
 //   }
 // }
+
+
+
+// import { NextResponse } from 'next/server'
+
+// export async function GET(request: Request, { params }: { params: { id: string } }) {
+//   try {
+//     const response = await fetch(`https://mastercrypto.org/posts/${params.id}/`)
+//     const data = await response.json()
+//     return NextResponse.json(data)
+//   } catch (error) {
+//     return NextResponse.json({ error: 'Failed to fetch blog post' }, { status: 500 })
+//   }
+// }
+
+// // export async function generateStaticParams() {
+// //   try {
+// //     // Fetch all post IDs from your API
+// //     const response = await fetch('https://mastercrypto.org/posts/')
+// //     const posts = await response.json()
+
+// //     // Return an array of objects, each with an 'id' property
+// //     return posts.map((post: { id: string }) => ({
+// //       id: post.id,
+// //     }))
+// //   } catch (error) {
+// //     console.error('Failed to fetch post IDs for static generation:', error)
+// //     return [] // Return an empty array if fetching fails
+// //   }
+// // }
 
