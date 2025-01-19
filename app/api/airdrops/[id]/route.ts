303f46eaab5dd6217c452973ce98250ed9061cbc
abela94 <abelbk007@gmail.com>
2025-01-19 12:11:08 +0300

    Update route.ts

--- app/api/airdrops/[id]/route.ts
+++ app/api/airdrops/[id]/route.ts
@@ -1,14 +1,14 @@
-// import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 
-// export async function GET(request: Request, { params }: { params: { id: string } }) {
-//   try {
-//     const response = await fetch(`https://mastercrypto.org/airdrops/${params.id}/`)
-//     const data = await response.json()
-//     return NextResponse.json(data)
-//   } catch (error) {
-//     return NextResponse.json({ error: 'Failed to fetch airdrop' }, { status: 500 })
-//   }
-// }
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  try {
+    const response = await fetch(`https://mastercrypto.org/airdrops/${params.id}/`)
+    const data = await response.json()
+    return NextResponse.json(data)
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch airdrop' }, { status: 500 })
+  }
+}
 
 
 
