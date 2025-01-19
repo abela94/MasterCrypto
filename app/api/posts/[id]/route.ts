3a034719b25835936902acf58245e3d28537c3b9
abela94 <abelbk007@gmail.com>
2025-01-19 12:11:33 +0300

    Update route.ts

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,14 +1,14 @@
-// import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 
-// export async function GET(request: Request, { params }: { params: { id: string } }) {
-//   try {
-//     const response = await fetch(`https://mastercrypto.org/posts/${params.id}/`)
-//     const data = await response.json()
-//     return NextResponse.json(data)
-//   } catch (error) {
-//     return NextResponse.json({ error: 'Failed to fetch blog post' }, { status: 500 })
-//   }
-// }
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  try {
+    const response = await fetch(`https://mastercrypto.org/posts/${params.id}/`)
+    const data = await response.json()
+    return NextResponse.json(data)
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch blog post' }, { status: 500 })
+  }
+}
 
 
 
